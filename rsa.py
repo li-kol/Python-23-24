d25fba87e4a4ca16a484f34a1da9108df8b97a17
li-kol <114173543+li-kol@users.noreply.github.com>
2024-01-06 21:12:59 +0300

    Добавлено решение

---
+++ rsa.py
@@ -0,0 +1,136 @@
+import random
+import typing as tp
+
+
+def is_prime(n: int) -> bool:
+    """
+    Tests to see if a number is prime.
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    # PUT YOUR CODE HERE
+    if n == 2:
+        return True
+    for i in range(2, n-1):
+        if n % i != 0:
+            return True
+        else:
+            return False
+
+
+def gcd(a: int, b: int) -> int:
+    """
+    Euclid's algorithm for determining the greatest common divisor.
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    # PUT YOUR CODE HERE
+    a_list_of_divisors = []
+    b_list_of_divisors = []
+    for i in range(1, a):
+        if a%i==0:
+            a_list_of_divisors.append(i)
+    for i in range(1, b):
+        if b%i==0:
+            b_list_of_divisors.append(i)
+    common_elements = list(set(a_list_of_divisors) & set(b_list_of_divisors))
+    gcd = max(common_elements)
+    if gcd == []:
+        return 1
+    else:
+        return gcd
+
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+
+    def egcd(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = egcd(b % a, a)
+            return (g, x - (b // a) * y, y)
+
+    g, x, y = egcd(e, phi)
+    if g != 1:
+        raise Exception('Modular inverse does not exist')
+    else:
+        return x % phi
+
+
+# PUT YOUR CODE HERE
+
+
+
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal")
+
+    # n = pq
+    # PUT YOUR CODE HERE
+    n = p*q
+    # phi = (p-1)(q-1)
+    # PUT YOUR CODE HERE
+    phi = (p - 1)*(q - 1)
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str: #function call synta  x
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m why mod? what purpose does it serve?
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string (plain)
+    return "".join(plain)
+
+
+if __name__ == "__main__":
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
